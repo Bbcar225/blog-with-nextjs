4d722617357d81f42a4a9e2b3b71e565c915d265
Ly Boubacar <59172284+Bbcar225@users.noreply.github.com>
2022-09-30 15:55:11 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // basePath: "/lab-ly/nextjs/blog",
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["ui-avatars.com"],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
